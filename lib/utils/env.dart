fd0e674f053f91edb86c6b2311d5a8efa83ae98d
nakatsinho <nakatsinho@gmail.com>
2021-10-25 06:52:54 +0200

    Improving my env call

--- lib/utils/env.dart
+++ lib/utils/env.dart
@@ -1,3 +1,3 @@
 const APP_URL = 'api.openweathermap.org/data/2.5/weather?q=';
-const API_KEY = '&appid=2f26f4026a31178c80d27a7b501e15a9';
+const API_KEY = '2f26f4026a31178c80d27a7b501e15a9';
 const UNITS = '&units=metric';
